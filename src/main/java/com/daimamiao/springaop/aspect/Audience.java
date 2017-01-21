f83a82444f8b7a11530ce018015f4aea5b455b89
daimamiao <ybzbxcc@gmail.com>
2017-01-22 00:27:18 +0800

    add

--- src/main/java/com/daimamiao/springaop/aspect/Audience.java
+++ src/main/java/com/daimamiao/springaop/aspect/Audience.java
@@ -3,5 +3,22 @@ package com.daimamiao.springaop.aspect;
 /**
  * Created by xcc on 1/21/2017.
  */
+
+
 public class Audience {
+    public void silenceCellPhones() {
+        System.out.println("Silencing cell phones");
+    }
+
+    public void takeSeats() {
+        System.out.println("Taking seats");
+    }
+
+    public void applause() {
+        System.out.println("CLAP CLAP CLAP!!!");
+    }
+
+    public void demandRefund() {
+        System.out.println("Demanding a refund");
+    }
 }
