8b3194e6db2950e144f2c0a62c0e2cc592bd26c6
daimamiao <ybzbxcc@gmail.com>
2017-01-22 00:25:53 +0800

    ready

---
+++ src/main/java/com/daimamiao/springaop/app/Performance.java
@@ -0,0 +1,8 @@
+package com.daimamiao.springaop.app;
+
+/**
+ * Created by xcc on 1/21/2017.
+ */
+public interface Performance {
+    public void perform();
+}
---
+++ src/main/java/com/daimamiao/springaop/aspect/Audience.java
@@ -0,0 +1,7 @@
+package com.daimamiao.springaop.aspect;
+
+/**
+ * Created by xcc on 1/21/2017.
+ */
+public class Audience {
+}
